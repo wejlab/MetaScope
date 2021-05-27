b04d2b672529833dc0f8df4ff0c6e7cdcc6bbafe
Rahul Varki <rahulvarki@varkis-MacBook-Pro.local>
2021-05-27 12:04:18 -0400

    New file for making bowtie index

--- R/mk_bowtie_index.R
+++ R/mk_bowtie_index.R
@@ -1,7 +1,7 @@
 #' Make a Bowtie2 index
 #' 
 #' This function is a wrapper for the \code{Rbowtie2::bowtie2_build} function.
-#' It will create large Bowtie2 indexes (.bt2l) from a fasta file.
+#' It will create large Bowtie2 indexes (.bt2l) from a reference fasta files.
 #' 
 #' @param ref_dir The name/location of the directory that contains the reference
 #' libraries in uncompressed fasta format
@@ -12,13 +12,23 @@
 #' @param threads The number of threads to be used. Default is 4.
 #' @param overwrite Whether existing files should be overwritten. Default is false.
 #' 
+#' @return Creates multiple large Bowtie2 indexes (.bt2l) for the supplied 
+#' reference .fasta files. Returns the file path of the folder holding these
+#' files.
+#' 
 #' @export
 
 
 mk_bowtie_index <- function(ref_dir, index_dir, index_name = basename(index_dir), threads = 4, overwrite=FALSE){
+  
+  ref_dir <- dir(ref_dir, full.names = TRUE)
+  index_dir <- tools::file_path_as_absolute(index_dir)
+  
   Rbowtie2::bowtie2_build(references = ref_dir, 
-                          bt2lIndex=file.path(index_dir, index_name),
+                          bt2Index=file.path(index_dir, index_name),
                           paste("--threads ",threads),
                           overwrite = overwrite
                           )
+  
+  return(paste("Bowtie2 index files located at ", tools::file_path_as_absolute(index_dir)))
 }
