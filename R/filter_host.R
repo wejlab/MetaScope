5a8ab962a6a677125685d8f2ae2d0959852df96a
rvarki <44979605+rvarki@users.noreply.github.com>
2021-10-10 19:32:18 -0400

    Added explicit call to ShortRead library to avoid error

--- R/filter_host.R
+++ R/filter_host.R
@@ -214,7 +214,7 @@ filter_host <- function(reads_bam, libs, lib_dir=NULL, output = paste(tools::fil
 
 
 filter_host_bowtie <- function(reads_bam, lib_dir, libs, output = paste(tools::file_path_sans_ext(reads_bam), "filtered", "bam", sep = "."), bowtie2_options = NULL, threads = 8, overwrite = FALSE){
-    # Note: Need to do library(ShortRead) otherwise get error: Error in occurrenceFilter(withSread = TRUE)  
+    suppressMessages(library(ShortRead)) #Error occurs within function below otherwise 
     
     # If no optional parameters are passed then use default parameters else use user parameters 
     if (missing(bowtie2_options))
