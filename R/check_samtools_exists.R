8c2d68cf41c6f6ee32120c75bc3d5dcf8a400fdd
Aubrey Odom <aodom@bu.edu>
2022-10-13 13:53:10 -0400

    Add function to check whether samtools exists

---
+++ R/check_samtools_exists.R
@@ -0,0 +1,10 @@
+#' @name check_samtools_exists
+#' @title Check if samtools exists on the system
+#' @description This is an internal function that is not meant to be used outside
+#' of the package. It checks whether samtools exists on the system.
+#' @return Returns TRUE if samtools exists on the system, else FALSE.
+
+check_samtools_exists <- function() {
+  if (file.exists(Sys.which("samtools"))) return(TRUE)
+  return(FALSE)
+}
